a596c3e4d5ffa646b90cf6a81ec4b9468f8b44fc
Neerav Patel <nickpatel.developer@gmail.com>
2021-08-18 11:37:03 -0400

    1 - simple string calculator

--- string-calculator.js
+++ string-calculator.js
@@ -1,8 +1,15 @@
 const Add = function (numbers) {
   
+  const number_arr = numbers.split(',');
+
   if (numbers.length === 0) {
     return 0;
   }
+
+  let sum = 0;
+
+  number_arr.map(num => sum += Number(num));
+  return sum;
 }
 
 module.exports = Add;
--- string-calculator.test.js
+++ string-calculator.test.js
@@ -1,5 +1,17 @@
 const add = require('./string-calculator');
 
-test('Empty string should return 0', () => {
-  expect(add('')).toBe(0);
+describe('String Calculator', () => {
+  test('Empty string should return 0', () => {
+    expect(add('')).toBe(0);
+  });
+
+  test('Return type should be integer', () => {
+    expect(add('1,2,5')).not.toBeNaN();
+  });
+
+  it('Return 8 when "1,2,5"', () => {
+    expect(add('1,2,5')).toBe(8);
+  });
 });
+
+
